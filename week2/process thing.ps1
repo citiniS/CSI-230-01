944979c53917733fd7a6d304fa0b51c526137de9
citiniS <160625645+citiniS@users.noreply.github.com>
2024-09-09 17:22:45 -0400

    Update process thing.ps1

--- week2/process thing.ps1
+++ week2/process thing.ps1
@@ -1,13 +1,3 @@
-﻿clear
-#Q1
-#(Get-Process | Where-Object { $_.ProcessName -ilike "c*"})
-#Q2
-#(Get-Process | Where-Object { $_.Path  -inotlike "*system32*"})
-#Q3
-#Get-Service | Where-Object { $_.Status -ine "Running"} | Sort-Object | Export-Csv -Path .\Processes
-#Q4
-
-
 $chrome = (Get-Process | Where-Object { $_.ProcessName -ilike "chrome"})
 if($chrome -eq $null)
 {
