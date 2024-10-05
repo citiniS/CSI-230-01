e0d28e9716ba017f9d636617f46747ad07de4fdc
Yin Bo <yinbo.chen@mymail.champlain.edu>
2024-10-05 18:30:33 -0400

    commit for week 6

---
+++ week6/TurnToMenu.ps1
@@ -0,0 +1,55 @@
+﻿clear
+.'C:\Users\champuser\CSI-230-01\week2\Question 4 code.ps1'
+. C:\Users\champuser\CSI-230-01\week6\Event-Logs.ps1
+. C:\Users\champuser\CSI-230-01\week4\Apache-Logs.ps1
+#. C:\Users\champuser\CSI-230-01\week6\main.ps1
+
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Display Last 10 Apache Logs`n"
+$Prompt += "2 - Display Last 10 Failed Log-In Logs`n"
+$Prompt += "3 - List at Risk Users`n"
+$Prompt += "4 - Open/Close Champlain College Chrome Tab`n"
+$Prompt += "5 - Exit`n"
+
+$operation = $true
+
+while($operation){
+ 
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+    if($choice -eq 1){
+        $somethingTable = apacheLogs "/index.html" '304' "*Mozilla*" | Select-Object -Last 10
+        $somethingTable 
+    }
+
+    elseif($choice -eq 2){
+        $failedLoginsTable = getFailedLogins 100 | Select-Object -Last 10
+        $failedLoginsTable
+    }
+
+    elseif($choice -eq 3){
+        getAtRiskUser
+    }
+
+    elseif($choice -eq 4){
+        $chromeTab = chromeOperation
+        $chromeTab
+    }
+
+
+    elseif($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -ne '[1-5]'){
+        Write-Host "`nIncorrect input, please try again"
+    }
+
+}
+
+
